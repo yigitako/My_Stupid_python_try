3d31bd7fbd1ea3c44c3f75b21d591a10560da505
yigitako <51721029+yigitako@users.noreply.github.com>
2021-09-08 00:29:03 +0300

    Add files via upload

---
+++ st.py
@@ -0,0 +1,36 @@
+#Firs # QUESTION: Number 1
+print("Lütfen bir sayı giriniz ")
+_first_Input =(int(input(":>")))
+if float(_first_Input) < float():
+    print("Negatif bir sayı girdiniz")
+    quit()
+#Second # QUESTION: Number 2
+print("2.inci bir sayı giriniz")
+_Second_input=(int(input(":>")))
+#sum of the Number 1 and 2
+print("sayıların toplamı")
+print(_Second_input + _first_Input)
+print("Please give a good Number")
+#favorite drink
+drink_list = [
+"1)coke",
+"2)pepsi",
+"3)coke light"]
+print("what is your favorite drink")
+print(drink_list)
+#food list and food selection
+food=(int(input(":>")))
+print("what would you like to eat ?")
+menu =[
+      "1)pizza"
+      "2)hamburger"
+      "3)lahmacun"
+      "4)kebap"
+      ]
+print(menu)
+input(":>")
+if food > int("5"):
+    print("that is not a valid choice")
+    quit()
+else:
+    print("bon apetit")
